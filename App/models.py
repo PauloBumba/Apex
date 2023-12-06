f240402abe6675f148d28f17f30a8e43de3c1cf0
Paulo418566 <paulomvbumba@gmail.com>
2023-12-06 19:30:12 -0300

    Apex

--- App/models.py
+++ App/models.py
@@ -1,14 +1,3 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class user(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    senha = db.Column(db.String(60), nullable=False)
-    usuario_id = db.Column(db.String(20), unique=True, nullable=False)
-    cpf = db.Column(db.String(11), unique=True, nullable=False)
-
 
 
 
